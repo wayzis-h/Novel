f49312c7d58e4fc793443ddaa2cf0580fef32f4f
Wayzis <25618051+wayzis-h@users.noreply.github.com>
2023-02-14 23:07:56 +0300

    Add a germ for a text clipping mechanism.

--- Schizofascism.Desktop/Novel.cs
+++ Schizofascism.Desktop/Novel.cs
@@ -16,7 +16,9 @@ namespace Schizofascism.Desktop
         // Temporary variables for prototype only.
         MgPrimitiveBatcher _batcher;
         private SpriteFont t_sf_font;
-        private Texture2D t_texture;
+        private Texture2D t_white;
+        private Texture2D t_black;
+        private Texture2D t_transparent;
         private Vector2 t_windowSize;
         private Texture2D t_bg;
         private string text = "Привет,\n Мир!";
@@ -29,6 +31,7 @@ namespace Schizofascism.Desktop
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            _graphics.PreferredDepthStencilFormat = DepthFormat.Depth24Stencil8;
 #if !DEBUG
             _graphics.IsFullScreen = true;
             _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
@@ -48,10 +51,16 @@ namespace Schizofascism.Desktop
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            System.GC.Collect(0);
+
             // TODO: use this.Content to load your game content here
             t_sf_font = Content.Load<SpriteFont>("Fonts/LucidaSans");
-            t_texture = new Texture2D(_graphics.GraphicsDevice, 1, 1);
-            t_texture.SetData<Color>(new[] { Color.White });
+            t_black = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+            t_black.SetData<Color>(new[] { Color.Black });
+            t_white = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+            t_white.SetData<Color>(new[] { Color.White });
+            t_transparent = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+            t_transparent.SetData<Color>(new[] { Color.Transparent });
             t_windowSize = new Vector2(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
             t_bg = Content.Load<Texture2D>("Backgrounds/Ray");
             t_mc = Content.Load<Texture2D>("Sprites/MainChar");
@@ -76,23 +85,52 @@ namespace Schizofascism.Desktop
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(ClearOptions.Target | ClearOptions.Stencil, Color.CornflowerBlue, 0, 0);
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin(samplerState: SamplerState.PointWrap);
-
             _spriteBatch.Draw(t_bg, new Rectangle(Point.Zero, t_windowSize.ToPoint()), Color.White);
             _spriteBatch.Draw(t_mc, new Rectangle(t_prewState.Position, new Point(77*2, 220*2)), new Rectangle(Point.Zero, new Point(77, 220)), Color.White);
+            _spriteBatch.End();
+
+            //
+            var m = Matrix.CreateOrthographicOffCenter(0,
+                _graphics.GraphicsDevice.PresentationParameters.BackBufferWidth,
+                _graphics.GraphicsDevice.PresentationParameters.BackBufferHeight,
+                0, 0, 1);
+            var a = new AlphaTestEffect(_graphics.GraphicsDevice) { Projection = m };
+
+            var s1 = new DepthStencilState
+            {
+                StencilEnable = true,
+                StencilFunction = CompareFunction.Always,
+                StencilPass = StencilOperation.Replace,
+                ReferenceStencil = 1,
+                DepthBufferEnable = false,
+            };
+            var s2 = new DepthStencilState
+            {
+                StencilEnable = true,
+                StencilFunction = CompareFunction.LessEqual,
+                StencilPass = StencilOperation.Keep,
+                ReferenceStencil = 1,
+                DepthBufferEnable = false,
+            };
+
+            _spriteBatch.Begin(sortMode: SpriteSortMode.Immediate, depthStencilState: s1/*, effect: a*/);
+            _spriteBatch.Draw(t_transparent, new Rectangle(t_prewState.Position.X - 40, t_prewState.Position.Y - 40, 35, 35), Color.Transparent);
+            _spriteBatch.End();
+
+            _spriteBatch.Begin(sortMode: SpriteSortMode.Immediate, depthStencilState: s2/*, effect: a*/);
             Vector2 ts;
             Vector2 pos = t_windowSize / 2;
-            foreach (var line in text.Split(new[] {'\n'}))
+            foreach (var line in text.Split(new[] { '\n' }))
             {
                 ts = t_sf_font.MeasureString(line);
                 pos.Y += ts.Y - 4;
-                _spriteBatch.Draw(t_texture, new Rectangle(pos.ToPoint(), ts.ToPoint()), new Color(new Vector4(0.3f)));
+                _spriteBatch.Draw(t_white, new Rectangle(pos.ToPoint(), ts.ToPoint()), new Color(new Vector4(0.3f)));
                 _spriteBatch.DrawString(t_sf_font, line, pos, Color.Black);
             }
-
             _spriteBatch.End();
 
             //_batcher.FillCircle(new Vector2(30), 20.23f, new Color(Color.LemonChiffon, 0.4f), 10);
